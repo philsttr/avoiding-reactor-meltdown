949a6deb757f6785763f27f681102c38cd712ad1
Phil Clay <philsttr@users.noreply.github.com>
2020-05-27 11:38:11 -0700

    Fixes #1 Add good versions of BadExamples

--- src/main/java/meltdown/demo/BadExamples.java
+++ src/main/java/meltdown/demo/BadExamples.java
@@ -1,5 +1,8 @@
 package meltdown.demo;
 
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
 /**
  * Examples found in code reviews.
  * Names have been changed.
@@ -7,6 +10,7 @@ package meltdown.demo;
 public class BadExamples {
 
 	/*
+	BAD
 	public Mono<Void> removeFooBar(final long fooId, final long barId) {
 		try {
 			barClientService.removeBar(fooId, barId);
@@ -14,6 +18,15 @@ public class BadExamples {
 			return Mono.error(new ApplicationException());
 		}
 		return barNotifier.sendRemoveBarEvent(barId);
+	}
+
+	GOOD
+	public Mono<Void> removeFooBar(final long fooId, final long barId) {
+		return Mono.fromRunnable(() -> barClientService.removeBar(fooId, barId))
+			// if removeBar is blocking, subscribe on a bounded elastic scheduler
+			.subscribeOn(Schedulers.boundedElastic())
+			.onErrorMap(ApplicationException::new)
+			.then(barNotifier.sendRemoveBarEvent(barId));
 	}
 	 */
 }
